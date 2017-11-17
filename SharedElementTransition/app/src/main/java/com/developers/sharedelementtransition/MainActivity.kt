7eec70a7f57e8b1b2422889b0e729832dde6bc68
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-11-18 04:38:35 +0530

    feat: sharedElementTransition example added

---
+++ SharedElementTransition/app/src/main/java/com/developers/sharedelementtransition/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.developers.sharedelementtransition
+
+import android.content.Intent
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        card_view.setOnClickListener {
+            val intent = Intent(this, TransitionActivity::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val options = ActivityOptionsCompat
+                        .makeSceneTransitionAnimation(this,
+                                card_view as View, getString(R.string.transition_string))
+                startActivity(intent, options.toBundle())
+            } else {
+                startActivity(intent)
+            }
+        }
+
+    }
+}
---
+++ SharedElementTransition/app/src/main/java/com/developers/sharedelementtransition/TransitionActivity.kt
@@ -0,0 +1,12 @@
+package com.developers.sharedelementtransition
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TransitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transition)
+    }
+}
