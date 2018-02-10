71b926eaa90736cfa11568d8f0c0d9dcbaae6569
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2018-02-10 19:22:12 +0530

    feat: added object animator example

---
+++ ObjectAnimator/README.md
@@ -0,0 +1,6 @@
+# ObjectAnimators
+Object Animators are the subclass of ValueAnimator but they make animating properties of views more easy.
+<br><br>
+<b>Screenshots:</b>
+<br><br>
+<img src="object.gif"/>
---
+++ ObjectAnimator/app/src/main/java/com/developers/objectanimator/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.developers.objectanimator
+
+import android.animation.ObjectAnimator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        animate_button.setOnClickListener({
+            val animator = ObjectAnimator.ofFloat(textView, "textSize", 12f)
+            animator.duration = 800
+            animator.start()
+        })
+    }
+}
