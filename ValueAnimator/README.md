788f1e04184292a93056ccc8000011ce1250f0dd
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2018-02-10 18:18:58 +0530

    feat: added value animator example

---
+++ ValueAnimator/README.md
@@ -0,0 +1,6 @@
+# ValueAnimators
+ValueAnimators provide the timing engine which we can use to get the animated values and can set to any property of views or in any other way.In this example we animate the textSize of TextView using ValueAnimators.
+<br><br>
+Screenshots:
+<br><br>
+<img src="value.gif"/>
---
+++ ValueAnimator/app/src/main/java/com/developers/valueanimator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.developers.valueanimator
+
+import android.animation.ValueAnimator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val endValue = 20
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        animate_button.setOnClickListener({
+            val valueAnimator = ValueAnimator.ofFloat(textView.textSize, endValue.toFloat())
+            valueAnimator.duration = 600
+            valueAnimator.addUpdateListener {
+                val animatedValue = it.animatedValue as Float
+                println(animatedValue)
+                textView.textSize = animatedValue
+            }
+            valueAnimator.start()
+        })
+    }
+}
