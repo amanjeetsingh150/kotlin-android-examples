8bd540b978e1a963e7592a82a4d3b99e88bee3b4
Anshdeep Singh <anshdeep.1494@gmail.com>
2018-02-21 14:54:45 +0530

    added bottom navigation view example

---
+++ BottomNavigationView/app/src/main/java/com/anshdeep/bottomnavigationview/FragmentHome.kt
@@ -0,0 +1,30 @@
+package com.anshdeep.bottomnavigationview
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by ansh on 21/02/18.
+ */
+class FragmentHome : Fragment() {
+
+    /**
+     * Initialize newInstance for passing parameters
+     */
+    companion object {
+        fun newInstance(): FragmentHome {
+            val fragmentHome = FragmentHome()
+            val args = Bundle()
+            fragmentHome.arguments = args
+            return fragmentHome
+        }
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_home, container, false)
+    }
+}
---
+++ BottomNavigationView/app/src/main/java/com/anshdeep/bottomnavigationview/FragmentNotification.kt
@@ -0,0 +1,17 @@
+package com.anshdeep.bottomnavigationview
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by ansh on 21/02/18.
+ */
+class FragmentNotification : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_notification, container, false)
+    }
+}
