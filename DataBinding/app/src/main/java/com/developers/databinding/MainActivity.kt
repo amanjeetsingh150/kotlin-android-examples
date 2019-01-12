e9fd1e70c33898e6012f89bf808be39f317dcbfb
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2019-01-12 17:01:00 +0530

    updated: dependencies and compiler of databinding with androidx

--- DataBinding/app/src/main/java/com/developers/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/developers/databinding/MainActivity.kt
@@ -9,9 +9,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding:ActivityMainBinding=DataBindingUtil
-                .setContentView(this,R.layout.activity_main)
-        val user=User("Amanjeet Singh",21,"amanjeetsingh150@gmail.com")
+        val binding: ActivityMainBinding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main)
+        val user = User("Amanjeet Singh", 21, "amanjeetsingh150@gmail.com")
         binding.setVariable(BR.user, user)
     }
 }
