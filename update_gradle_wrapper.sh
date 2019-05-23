4646fb3cfabb35102142f3a917ebbba6cc6e873f
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2019-05-24 01:01:48 +0530

    update: gradle plugin to 5.1.1

---
+++ update_gradle_wrapper.sh
@@ -0,0 +1,19 @@
+echo "Update gradle wrapper to which version?"
+read version
+
+# Iterate over each child directory inside the current directory
+echo "# Updating to Gradle Wrapper Version: $version"
+echo ""
+for dir in */;
+do
+	echo ""
+	echo ">>>> Updating directory : " $dir
+	(cd "$dir" && ./gradlew clean && ./gradlew wrapper --gradle-version $version --distribution-type all && ./gradlew);
+	echo ""
+	echo ">>>> Done."
+done
+
+# Delete all generated build folders, because they will eat up a lot of space on the disc
+echo "# Deleting all build folders"
+echo ""
+./delete_build_folder.sh 
