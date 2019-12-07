d404cfae0f0aeb11d0a4984f377e6adb3db50f13
Amanjeet Singh <amanjeetsingh150@gmail.com>
2019-12-08 01:39:50 +0530

    Added Motion Layout example and remove redundant folder

---
+++ MotionLayout/app/src/main/java/com/developers/motionlayout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.developers.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        motionLayout.setTransitionListener(object : TransitionAdapter() {
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                when (currentId) {
+                    R.id.offScreenPass,
+                    R.id.offScreenLike -> {
+                        motionLayout.progress = 0f
+                        motionLayout.setTransition(R.id.rest, R.id.like)
+                    }
+                }
+            }
+
+        })
+    }
+}
